5681d9decd411b5df4d81a7de97dbc3749deaa88
Pierre Ph@rel <edo.tensei095@gmail.com>
2020-03-29 22:33:24 +0100

    [msgpack] sbuffer implementation

---
+++ msgpack/include/object.hpp
@@ -0,0 +1,19 @@
+#ifndef ARK_MSGPACK_OBJ_HPP
+#define ARK_MSGPACK_OBJ_HPP
+
+#include <variant>
+#include <iostream> 
+#include <sstream>
+#include <msgpack.hpp>
+#include <Ark/Module.hpp>
+
+using CObject = std::variant<bool, double, std::string, std::vector<Value>>;
+extern CObject get_cobject(const Value &ark_object, ValueType type);
+namespace ArkMsgpack
+{
+	// msgpack::object_handle
+	extern Value msgpack_obj_handle(std::vector<Value> &args);
+	extern Value msgpack_sbuffer(std::vector<Value> &args);
+}
+
+#endif
