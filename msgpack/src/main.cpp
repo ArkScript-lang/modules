3869aa88714152c42a5324cbed26bcdd91935c95
Pierre Ph@rel <edo.tensei095@gmail.com>
2020-03-31 21:56:50 +0100

    [msgpack] objects add on

--- msgpack/src/main.cpp
+++ msgpack/src/main.cpp
@@ -9,5 +9,9 @@ ARK_API_EXPORT Mapping_t getFunctionsMapping()
     map["msgpackMiniObjStr"] = ArkMsgpack::Minimal::object_str;
     // msgpack objects constructors
     map["msgpackObjHandle"] = ArkMsgpack::msgpack_obj_handle;
+    map["msgpackObj"] = ArkMsgpack::msgpack_obj;
+    map["msgpackSBuffer"] = ArkMsgpack::msgpack_sbuffer;
+    map["msgpackPack"] = ArkMsgpack::pack;
+    map["msgpackUnpack"] = ArkMsgpack::unpack;
     return map;
 }
--- msgpack/src/object.cpp
+++ msgpack/src/object.cpp
@@ -33,18 +33,37 @@ namespace ArkMsgpack
 	Value msgpack_sbuffer(std::vector<Value> &args)
 	{
 		if(args.size() != 0)
-			throw std::runtime_error("ArgError : No arguments for this function");
+			throw std::runtime_error("ArgError : No arguments are necessary for this function");
 		static msgpack::sbuffer sbuf;
 
 		return Value(UserType(&sbuf));
 	}
-	// msgpack::object_handle
+
+	// msgpack::object_handle and msgpack::object
 	Value msgpack_obj_handle(std::vector<Value> &args)
 	{
 		if(args.size() != 1)
-			throw std::runtime_error("ArgError : This function must have 1 arguments");
-		msgpack::object_handle oh;
+			throw std::runtime_error("ArgError : This function must have 1 argument");
+        if(args[0].valueType() != ValueType::User || args[0].usertype_ref().type_id() != std::type_index(typeid(msgpack::sbuffer)))
+            throw Ark::TypeError("The packed buffer must be a msgpack::sbuffer");
+		static msgpack::object_handle oh;
 
+		msgpack::sbuffer* sbuf {static_cast<msgpack::sbuffer*>((args[0]).usertype().data())};
+		oh = msgpack::unpack(sbuf->data(), sbuf->size());
+
+		return Value(UserType(&oh));
+	}
+
+	Value msgpack_obj(std::vector<Value> &args)
+	{
+		if(args.size() != 1)
+			throw std::runtime_error("ArgError : This function must have 1 argument");
+        if(args[0].valueType() != ValueType::User || args[0].usertype_ref().type_id() != std::type_index(typeid(msgpack::object_handle)))
+            throw Ark::TypeError("The packed buffer must be a msgpack::object_handle");
+		static msgpack::object o;
+
+		msgpack::object_handle* oh {static_cast<msgpack::object_handle*>((args[0]).usertype().data())};
+		oh = msgpack::unpack(sbuf->data(), sbuf->size());
 
 		return Value(UserType(&oh));
 	}
