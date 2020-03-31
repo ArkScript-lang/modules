8b7151487dd0e8d7e195818e0b4af4d5474c06cb
Pierre Ph@rel <edo.tensei095@gmail.com>
2020-03-31 21:56:08 +0100

    [msgpack] update adaptor

--- msgpack/include/adaptor.hpp
+++ msgpack/include/adaptor.hpp
@@ -25,39 +25,33 @@ namespace msgpack
 					}
 					else
 					{
-						Value lst {ValueType::List};
+						Value lst_v {ValueType::List};
 
 						for(unsigned i {0}; i < o.via.array.size; ++ i)
 						{
-							Value ark_object;
+							Value ark_object_v;
+
+							// number
 							try
 							{
-								ark_object = Value(o.via.array.ptr[i].via.f64);
+								ark_object_v = Value(o.via.array.ptr[i].via.f64);
 							}
 							catch(const std::bad_cast &e)
 							{
-
+								// string
 								try
 								{
-									ark_object = Value(std::string {o.via.array.ptr[i].via.str.ptr});
+									ark_object_v = Value(std::string {o.via.array.ptr[i].via.str.ptr});
 								}
+							
 								catch(const std::bad_cast &e) 
 								{
-									/*
-									try
-									{
-										for(unsigned j = 0; j < o.via.array.ptr[i].via.array.size; ++ j)
-										{
-
-										}
-									}static_cast<Value>(v).list()
-									catch(const std::bad_cast &e) {}*/
-								} // sub list convert
+								} // TODO : sub list convert
 							}
 
-							lst.push_back(ark_object);
+							lst_v.push_back(ark_object_v);
 						}
-						v = lst;
+						v = lst_v;
 					}
 					
 					return o;
@@ -96,53 +90,6 @@ namespace msgpack
 					return o;
 				}
 			};
-
-/*
-			// ark value msgpack object implementation
-			template <> struct object<Value> 
-			{
-				void operator()(msgpack::object& o, Value const& v) const 
-				{
-					if(v.valueType() == ValueType::NFT)
-					{
-						o.type = type::BOOLEAN; 
-						o.via.boolean = static_cast<Value>(v).number();
-					}
-					else if(v.valueType() == ValueType::Number)
-					{
-						o.type = type::FLOAT64;
-						o.via.f64 = static_cast<Value>(v).number();
-					}
-					else
-					{
-						std::vector<Value> lst {static_cast<Value>(v).list()};
-
-						o.type = type::ARRAY;
-						o.via.array.size = lst.size();
-						o.via.array.ptr = static_cast<msgpack::object*>(std::malloc(sizeof(msgpack::object) * o.via.array.size));
-						
-						for(unsigned i {0}; i < o.via.array.size; ++ i) 
-						{
-							if(lst[i].valueType() == ValueType::NFT)
-							{
-								o.via.array.ptr[i].type = type::BOOLEAN;
-								o.via.array.ptr[i].via.f64 = static_cast<Value>(lst[i]).number();
-							}
-							else if(lst[i].valueType() == ValueType::Number)
-							{
-								o.via.array.ptr[i].type = type::FLOAT64;
-								o.via.array.ptr[i].via.f64 = static_cast<Value>(lst[i]).number();								
-							}
-							else
-							{
-								o.via.array.ptr[i].type = type::STR;
-								o.via.array.ptr[i].via.str.ptr = static_cast<Value>(lst[i]).string_ref().c_str();							
-							}
-						}
-					}
-				}
-			};
-*/	
 		}
 	}
 }
