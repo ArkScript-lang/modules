412e7ca090b261fd8dc206b5e2ff82c4db839880
PierrePh@rel <edo.tensei095@gmail.com>
2021-06-21 13:49:46 +0200

    try to fix

--- msgpack/src/main.cpp
+++ msgpack/src/main.cpp
@@ -1,18 +1,19 @@
+#include <Ark/Module.hpp>
 #include <ark_msgpack.hpp>
 
-ARK_API_EXPORT Mapping_t getFunctionsMapping()
+ARK_API_EXPORT mapping* getFunctionsMapping()
 {
     mapping* map = mapping_create(6);
 
     // msgpack objects constructors
-    map["msgpack:sbuffer"] = msgpack_sbuffer;
-    map["msgpack:objectHandle"] = msgpack_obj_handle;
-    map["msgpack:object"] = msgpack_obj;
+    mapping_add(map[0], "msgpack:sbuffer", msgpack_sbuffer);
+    mapping_add(map[1], "msgpack:objectHandle", msgpack_obj_handle);
+    mapping_add(map[2], "msgpack:object", msgpack_obj);
 
     // msgpack operations
-    map["msgpack:pack"] = msgpack_pack;
-    map["msgpack:unpack"] = msgpack_unpack;
-    map["msgpack:convert"] = msgpack_convert;
+    mapping_add(map[3], "msgpack:pack", msgpack_pack);
+    mapping_add(map[4], "msgpack:unpack", msgpack_unpack);
+    mapping_add(map[5], "msgpack:convert", msgpack_convert);
 
     return map;
 }
