f3b64582777974e40c1f302e9917c560a2abeb82
Ray Alovera <ralovera@stackify.com>
2021-06-25 16:02:28 +0800

    c bit manipulation for left and right shift, xor

--- bitwise/src/main.cpp
+++ bitwise/src/main.cpp
@@ -3,146 +3,58 @@
 
 namespace ArkBitwise
 {
-
-    std::vector<std::unique_ptr<std::bitset<8>>>& getBitwiseObject()
-    {
-        static std::vector<std::unique_ptr<std::bitset<8>>> bitsetObject;
-        return bitsetObject;
-    }
-
-    UserType::ControlFuncs* getCfs()
-    {
-        static UserType::ControlFuncs cfs;
-        cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
-            os << a.as<std::bitset<8>>();
-            return os;
-        };
-        cfs.deleter = [](void* data) {
-            std::vector<std::unique_ptr<std::bitset<8>>>& bitsets = getBitwiseObject();
-            auto it = std::find_if(bitsets.begin(), bitsets.end(), [data](const auto& val) -> bool {
-                return val.get() == static_cast<std::bitset<8>*>(data);
-            });
-            if (it != bitsets.end())
-                bitsets.erase(it);
-        };
-        return &cfs;
-    }
-
-    Ark::Value bitset2Ark(std::bitset<8>& bitset)
-    {
-        std::bitset<8>* ptr = getBitwiseObject().emplace_back(std::make_unique<std::bitset<8>>(bitset)).get();
-        Value v = Ark::Value(Ark::UserType(ptr));
-        v.usertypeRef().setControlFuncs(getCfs());
-        return v;
-    }
-
-    Ark::Value convert2Bitset(unsigned long long val)
-    {
-        std::bitset<8>* ptr = getBitwiseObject().emplace_back(std::make_unique<std::bitset<8>>(val)).get();
-        Value v = Ark::Value(Ark::UserType(ptr));
-        v.usertypeRef().setControlFuncs(getCfs());
-        return v;
-    }
-
-    Ark::Value convert2Bitset(std::string val,
-                                   unsigned long long pos = 0,
-                                   unsigned long long len = 0)
-    {
-        std::bitset<8>* ptr = getBitwiseObject().emplace_back(std::make_unique<std::bitset<8>>(val, pos, len)).get();
-        Value v = Ark::Value(Ark::UserType(ptr));
-        v.usertypeRef().setControlFuncs(getCfs());
-        return v;
-    }
-
-    Ark::Value makeBitset(std::vector<Value>& n, Ark::VM* vm)
-    {
-        if (n.size() < 1)
-            throw std::runtime_error("bitwise:makeBitset need at least a single argument: data");
-        else if (n.size() > 3)
-            throw std::runtime_error("bitwise:makeBitset can only take up to 3 arguments");
-        Ark::Value v;
-        ValueType nType = n[0].valueType();
-        /*
-        if (nType != ValueType::String && nType != ValueType::Number)
-            throw std::runtime_error("bitwise:makeBitset: the first argument can only be a String or a Number");
-*/
-        if (nType == ValueType::Number)
-        {
-            unsigned long long num = static_cast<long>(n[0].number());
-            v  = convert2Bitset(num);
-        }
-        else if (nType == ValueType::String)
-        {
-
-            std::string src = n[0].stringRef().toString();
-            unsigned long long pos, len;
-            try
-            {
-                if (n.size() == 2 && n[1].valueType() == ValueType::Number)
-                {
-                    pos = static_cast<long>(n[1].number());
-                    v = convert2Bitset(src, pos);
-                }
-                else if (n.size() == 3 && n[1].valueType() == ValueType::Number && n[2].valueType() == ValueType::Number)
-                {
-                    pos = static_cast<long>(n[1].number());
-                    len = static_cast<long>(n[2].number());
-                    v = convert2Bitset(src, pos, len);
-                }
-            }
-            catch (const std::out_of_range e)
-            {
-                throw std::runtime_error("bitwise:makeBitset: ");
-            }
-        } 
-        else
-        {
-            return Ark::Nil;
-        }
-        return v;
-    }
-
     Value rshift(std::vector<Value>& n, Ark::VM* vm)
     {
+
         if (n.size() != 2)
-            throw std::runtime_error("bitwise:rshift need at least 2 arguments: bitset, pos");
+            throw std::runtime_error("bitwise:rshift: needs 1 argument: number");
+        if (n[0].valueType() != ValueType::Number 
+            && n[1].valueType() != ValueType::Number)
+            throw Ark::TypeError("bitwise:rshift the 2 arguments must be numbers"); 
 
-        if (n[0].valueType() != ValueType::User ||
-                !n[0].usertype().is<std::bitset<8>>() )
-            throw std::runtime_error("bitwise:rshift the first argument can only be a bitset");
-
-        if (n[1].valueType() != ValueType::Number)
-            throw std::runtime_error("bitwise:rshift the second argument can only be a Number");
-
-        std::bitset<8>& bitsetObj = n[0].usertypeRef().as<std::bitset<8>>();
-        std::bitset<8>& shifted = bitsetObj >> n[1].number();
-        return bitset2Ark(shifted);
+        int num = static_cast<int>(n[0].number());
+        int shift = static_cast<int>(n[1].number());
+        int shifted = num >> shift;
+        return Value(shifted);
     }
 
     Value lshift(std::vector<Value>& n, Ark::VM* vm)
     {
+   
         if (n.size() != 2)
-            throw std::runtime_error("bitwise:lshift need at least 2 arguments: bitset, pos");
+            throw std::runtime_error("bitwise:lshift: needs 1 argument: number");
+        if (n[0].valueType() != ValueType::Number 
+            && n[1].valueType() != ValueType::Number)
+            throw Ark::TypeError("bitwise:lshift the 2 arguments must be numbers"); 
 
-        if (n[0].valueType() != ValueType::User ||
-                !n[0].usertype().is<std::bitset<8>>() )
-            throw std::runtime_error("bitwise:lshift the first argument can only be a bitset");
-
-        if (n[1].valueType() != ValueType::Number)
-            throw std::runtime_error("bitwise:rshift the second argument can only be a Number");
-
-        std::bitset<8>& bitsetObj = n[0].usertypeRef().as<std::bitset<8>>();
-        std::bitset<8>& shifted = bitsetObj << n[1].number();
-        return bitset2Ark(shifted);
+        int num = static_cast<int>(n[0].number());
+        int shift = static_cast<int>(n[1].number());
+        std::clog << num << std::endl;
+        int shifted = num << shift;
+        return Value(shifted);
     }
+    
+    Value xor(std::vector<Value>& n, Ark::VM* vm)
+    {
 
+        if (n.size() != 2)
+            throw std::runtime_error("bitwise:xor: needs 1 argument: number");
+        if (n[0].valueType() != ValueType::Number 
+            && n[1].valueType() != ValueType::Number)
+            throw Ark::TypeError("bitwise:xor the 2 arguments must be numbers"); 
+
+        int num = static_cast<int>(n[0].number());
+        int shift = static_cast<int>(n[1].number());
+        int shifted = num ^ shift;
+        return Value(shifted);
+    }
 }
 
 ARK_API_EXPORT mapping *getFunctionsMapping()
 {
     mapping *map = mapping_create(3);
-    mapping_add(map[0], "bitwise:make-bitset", ArkBitwise::makeBitset);
-    mapping_add(map[1], "bitwise:rshift", ArkBitwise::rshift);
-    mapping_add(map[2], "bitwise:lshift", ArkBitwise::lshift);
+    mapping_add(map[0], "bitwise:rshift", ArkBitwise::rshift);
+    mapping_add(map[1], "bitwise:lshift", ArkBitwise::lshift);
+    mapping_add(map[2], "bitwise:xor", ArkBitwise::xor);
     return map;
 }
