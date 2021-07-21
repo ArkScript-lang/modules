1024472532bdf05b5bd3db8f3304404f31bf951e
Pierre Pharel <edo.tensei095@gmail.com>
2021-07-21 04:52:07 +0200

    update to usertype new standard

--- msgpack/include/object.hpp
+++ msgpack/include/object.hpp
@@ -2,18 +2,24 @@
 #define ARK_MSGPACK_OBJ_HPP
 
 #include <variant>
-/*
-#include <iostream>
-#include <sstream>
+#include <memory>
 #include <msgpack.hpp>
-*/
+
 #include <Ark/Module.hpp>
 
-using CObject = std::variant<bool, double, std::string, std::vector<Value>>;
-extern CObject get_cobject(Value& ark_object, ValueType type);
 
-extern Value msgpack_obj_handle(std::vector<Value>& args, VM* vm);
-extern Value msgpack_sbuffer(std::vector<Value>& args, VM* vm);
-extern Value msgpack_obj(std::vector<Value>& args, VM* vm);
+using CObject = std::variant<bool, double, std::string, std::vector<Value>>;
+
+std::list<std::unique_ptr<msgpack::sbuffer>>& get_sbuffers();
+std::list<std::unique_ptr<msgpack::object_handle>>& get_obj_handlers();
+std::list<std::unique_ptr<msgpack::object>>& get_objects();
+UserType::ControlFuncs* get_cfs_sbuffers();
+UserType::ControlFuncs* get_cfs_obj_handlers();
+UserType::ControlFuncs* get_cfs_objects();
+CObject get_cobject(Value& ark_object, ValueType type);
+
+Value msgpack_obj_handle(std::vector<Value>& args, VM* vm);
+Value msgpack_sbuffer(std::vector<Value>& args, VM* vm);
+Value msgpack_obj(std::vector<Value>& args, VM* vm);
 
 #endif
--- msgpack/src/object.cpp
+++ msgpack/src/object.cpp
@@ -1,6 +1,93 @@
 #include <object.hpp>
 #include <adaptor.hpp>
 
+std::list<std::unique_ptr<msgpack::sbuffer>>& get_sbuffers()
+{
+    static std::list<std::unique_ptr<msgpack::sbuffer>> sbuffers;
+    return sbuffers;
+}
+
+std::list<std::unique_ptr<msgpack::object_handle>>& get_obj_handlers()
+{
+    static std::list<std::unique_ptr<msgpack::object_handle>> object_handlers;
+    return object_handlers;
+}
+
+std::list<std::unique_ptr<msgpack::object>>& get_objects()
+{
+    static std::list<std::unique_ptr<msgpack::object>> objects;
+    return objects;
+}
+
+UserType::ControlFuncs* get_cfs_sbuffers()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackSBuffer<0x" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::sbuffer>>& sbuffers = get_sbuffers();
+        for (auto it = sbuffers.begin(), end = sbuffers.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                sbuffers.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
+UserType::ControlFuncs* get_cfs_obj_handlers()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackObjectHandler<0x" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::object_handle>>& object_handlers = get_obj_handlers();
+        for (auto it = object_handlers.begin(), end = object_handlers.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                object_handlers.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
+UserType::ControlFuncs* get_cfs_objects()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackObject<0x" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::object>>& objects = get_objects();
+        for (auto it = objects.begin(), end = objects.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                objects.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
 CObject get_cobject(Value& ark_object, ValueType type)
 {
     CObject object;
@@ -27,40 +114,49 @@ CObject get_cobject(Value& ark_object, ValueType type)
     return object;
 }
 
+/*******************************************************/
+/*              Objects constructors                   */
+/*******************************************************/
+
 // msgpack::sbuffer
 Value msgpack_sbuffer(std::vector<Value>& args, VM* vm)
 {
-    if(args.size() != 0)
-        throw std::runtime_error("ArgError : No arguments necessary for this function");
+    if (args.size() != 0)
+        throw std::runtime_error("ArgError: No arguments necessary for this function");
 
+    get_sbuffers().emplace_back(std::make_unique<msgpack::sbuffer>());
+    Value sbuffer = Value(UserType(get_sbuffers().back().get()));
+    sbuffer.usertypeRef().setControlFuncs(get_cfs_sbuffers());
 
-
-    return Value(UserType(t));
+    return sbuffer;
 }
 
-// msgpack::object_handle and msgpack::object
+// msgpack::object_handle
 Value msgpack_obj_handle(std::vector<Value>& args, VM* vm)
 {
-    if(args.size() != 1)
-        throw std::runtime_error("ArgError : This function must have 1 argument");
-    if(args[0].valueType() != ValueType::User || !(args[0].usertype().is<msgpack::sbuffer>()))
-        throw Ark::TypeError("msgpack:objectHandle The packed buffer must be a msgpack::sbuffer");
+    if (args.size() != 0)
+        throw std::runtime_error("ArgError: No arguments necessary for this function");
 
+    get_obj_handlers().emplace_back(std::make_unique<msgpack::object_handle>());
+    Value object_handle = Value(UserType(get_obj_handlers().back().get()));
+    object_handle.usertypeRef().setControlFuncs(get_cfs_obj_handlers());
 
-
-    return Value(UserType(t));
+    return object_handle;
 }
 
+// msgpack::object
 Value msgpack_obj(std::vector<Value>& args, VM* vm)
 {
-    if(args.size() != 1)
-        throw std::runtime_error("ArgError : This function must have 1 argument");
-    if(args[0].valueType() != ValueType::User || !(args[0].usertype().is<msgpack::object_handle>()))
-        throw Ark::TypeError("msgpack:object The packed buffer must be a msgpack::object_handle");
+    if (args.size() != 1)
+        throw std::runtime_error("ArgError: this function must have 1 argument");
+    if (args[0].valueType() != ValueType::User || !(args[0].usertype().is<msgpack::object_handle>()))
+        throw Ark::TypeError("msgpack:object: packed object must be a msgpack::object_handle");
 
-    msgpack::object* o;
+    msgpack::object_handle& oh = args[0].usertypeRef().as<msgpack::object_handle>();
+    get_objects().emplace_back(std::make_unique<msgpack::object>(oh.get()));
 
+    Value object = Value(UserType(get_objects().back().get()));
+    object.usertypeRef().setControlFuncs(get_cfs_objects());
 
-
-    return Value(UserType(o));
+    return object;
 }
