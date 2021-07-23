7b7ec007eec543f7dbcc8bae441bd89371e24144
Pierre Pharel <edo.tensei095@gmail.com>
2021-07-23 18:57:48 +0200

    module new files

---
+++ msgpack/documentation/Readme.md
@@ -0,0 +1,45 @@
+# msgpack module documentation
+
+A module for **msgpack**, an efficient binary serialization format like JSON, but smaller and faster .
+
+## **msgpack**
+
+
++ **msgpack:sbuffer()** : construct the buffer that must containes packed data ;
++ **msgpack:objectHandle()** : construct a msgpack::object_handle. It must manage and return msgpack::object ;
++ **msgpack:object(msgpack_object_handle)** : construct the msgpack::object. It must containe the unpacked object ;
++ **msgpack:pack(sbuf object)** : data packing ;
++ **msgpack:unpack()** : for data unpacking ;
++ **msgpack:convert()** : convert msgpack::object to ark usable object .
+
+
+
+### Example
+
+``` clojure
+(import "msgpack.arkm")
+
+# buffer
+(let sbuf (msgpack:sbuffer))
+
+# packing
+(msgpack:pack sbuf (list 1 2.45 "Yellow Bow !" true nil false (list 1 2.45 "hello !")))
+
+# unpacking
+# msgpack object handle
+(mut oh (msgpack:objectHandle))
+
+(msgpack:unpack sbuf oh)
+
+# msgpack object
+(let o (msgpack:object oh))
+
+# convert msgpackObj to ark object
+(mut dst (msgpack:convert o))
+(print "dst : " dst)
+```
+
+
+### Comming soon
+
++ Map pack, unpack and convert .
---
+++ msgpack/src/objects.cpp
@@ -0,0 +1,160 @@
+#include <objects.hpp>
+#include <adaptor.hpp>
+
+std::list<std::unique_ptr<msgpack::sbuffer>>& get_sbuffers()
+{
+    static std::list<std::unique_ptr<msgpack::sbuffer>> sbuffers;
+    return sbuffers;
+}
+
+std::list<std::unique_ptr<msgpack::object_handle>>& get_obj_handlers()
+{
+    static std::list<std::unique_ptr<msgpack::object_handle>> object_handlers;
+    return object_handlers;
+}
+
+std::list<std::unique_ptr<msgpack::object>>& get_objects()
+{
+    static std::list<std::unique_ptr<msgpack::object>> objects;
+    return objects;
+}
+
+UserType::ControlFuncs* get_cfs_sbuffers()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackSBuffer<" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::sbuffer>>& sbuffers = get_sbuffers();
+        for (auto it = sbuffers.begin(), end = sbuffers.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                sbuffers.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
+UserType::ControlFuncs* get_cfs_obj_handlers()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackObjectHandler<" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::object_handle>>& object_handlers = get_obj_handlers();
+        for (auto it = object_handlers.begin(), end = object_handlers.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                object_handlers.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
+UserType::ControlFuncs* get_cfs_objects()
+{
+    static UserType::ControlFuncs cfs;
+    cfs.ostream_func = [](std::ostream& os, const UserType& a) -> std::ostream& {
+        os << "msgpackObject<" << a.data() << ">";
+        return os;
+    };
+
+    cfs.deleter = [](void* data) {
+        std::list<std::unique_ptr<msgpack::object>>& objects = get_objects();
+        for (auto it = objects.begin(), end = objects.end(); it != end; ++ it)
+        {
+            if (&(*it) == data)
+            {
+                objects.erase(it);
+                break;
+            }
+        }
+    };
+
+    return &cfs;
+}
+
+CObject get_cobject(Value& ark_object, ValueType type)
+{
+    CObject object;
+
+    switch(type)
+    {
+        case ValueType::True:
+            object = true;
+            break;
+        case ValueType::False:
+            object = false;
+            break;
+        case ValueType::Number:
+            object = ark_object.number();
+            break;
+        case ValueType::String:
+            object = ark_object.stringRef().toString();
+            break;
+        case ValueType::List:
+            object = ark_object.list();
+            break;
+    }
+
+    return object;
+}
+
+/*              Objects constructors                   */
+
+// msgpack::sbuffer
+Value msgpack_sbuffer(std::vector<Value>& args, VM* vm)
+{
+    if (args.size() != 0)
+        throw std::runtime_error("ArgError: No arguments necessary for this function");
+
+    get_sbuffers().emplace_back(std::make_unique<msgpack::sbuffer>());
+    Value sbuffer = Value(UserType(get_sbuffers().back().get()));
+    sbuffer.usertypeRef().setControlFuncs(get_cfs_sbuffers());
+
+    return sbuffer;
+}
+
+// msgpack::object_handle
+Value msgpack_obj_handle(std::vector<Value>& args, VM* vm)
+{
+    if (args.size() != 0)
+        throw std::runtime_error("ArgError: No arguments necessary for this function");
+
+    get_obj_handlers().emplace_back(std::make_unique<msgpack::object_handle>());
+    Value object_handle = Value(UserType(get_obj_handlers().back().get()));
+    object_handle.usertypeRef().setControlFuncs(get_cfs_obj_handlers());
+
+    return object_handle;
+}
+
+// msgpack::object
+Value msgpack_obj(std::vector<Value>& args, VM* vm)
+{
+    if (args.size() != 1)
+        throw std::runtime_error("ArgError: this function must have 1 argument");
+    if (args[0].valueType() != ValueType::User || !(args[0].usertype().is<msgpack::object_handle>()))
+        throw Ark::TypeError("msgpack:object: packed object must be a msgpack::object_handle");
+
+    msgpack::object_handle& oh = args[0].usertypeRef().as<msgpack::object_handle>();
+    get_objects().emplace_back(std::make_unique<msgpack::object>(oh.get()));
+
+    Value object = Value(UserType(get_objects().back().get()));
+    object.usertypeRef().setControlFuncs(get_cfs_objects());
+
+    return object;
+}
