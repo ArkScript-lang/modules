c0dc0f1a4a5870eeb260ebd060b6fd5bcebf0b0a
Pierre Pharel <edo.tensei095@gmail.com>
2021-07-23 18:40:28 +0200

    adaptor now support sub string manipulation

--- msgpack/include/adaptor.hpp
+++ msgpack/include/adaptor.hpp
@@ -13,60 +13,57 @@ namespace msgpack
             // convert ark value
             template<> struct convert<Value>
             {
+                void convert_list(const msgpack::object_array o, Value& list_v) const
+                {
+                    for(std::size_t i = 0; i < o.size; ++ i)
+                    {
+                        Value v;
+
+                        if(o.ptr[i].type == type::NIL)
+                            v = Ark::Nil;
+                        else if(o.ptr[i].type == type::BOOLEAN)
+                            if(o.ptr[i].via.boolean == true)
+                                v = Ark::True;
+                            else
+                                v = Ark::False;
+                        else if(o.ptr[i].type == type::FLOAT)
+                            v = Value(o.ptr[i].via.f64);
+                        else if(o.ptr[i].type == type::STR)
+                        {
+                            std::size_t s = o.ptr[i].via.str.size;
+                            v = Value(std::string(o.ptr[i].via.str.ptr, s));
+                        }
+                        else if(o.ptr[i].type == type::ARRAY)
+                        {
+                            v = Value(ValueType::List);
+                            convert_list(o.ptr[i].via.array, v);
+                        }
+
+                        list_v.push_back(v);
+                    }
+                }
+
                 msgpack::object const& operator()(msgpack::object const& o, Value& v) const
                 {
                     if(o.type == type::NIL)
-                    {
                         v = Ark::Nil;
-                    }
                     else if(o.type == type::BOOLEAN)
-                    {
                         if(o.via.boolean == true)
                             v = Ark::True;
                         else
                             v = Ark::False;
-                    }
                     else if(o.type == type::FLOAT)
-                    {
                         v = Value(o.via.f64);
-                    }
                     else if(o.type == type::STR)
                     {
-                        v = Value(std::string {o.via.str.ptr});
+                        std::size_t s = o.via.str.size;
+                        v = Value(std::string(o.via.str.ptr, s));
                     }
                     else
                     {
-                        Value lst_v  = Ark::Value(ValueType::List);
-
-                        for(unsigned i  = 0; i < o.via.array.size; ++ i)
-                        {
-                            Value ark_object_v;
-
-                            if(o.via.array.ptr[i].type == type::NIL)
-                            {
-                                ark_object_v = Ark::Nil;
-                            }
-                            else if(o.via.array.ptr[i].type == type::BOOLEAN)
-                            {
-                                if(o.via.array.ptr[i].via.boolean == true)
-                                    ark_object_v = Ark::True;
-                                else
-                                    ark_object_v = Ark::False;
-                            }
-                            else if(o.via.array.ptr[i].type == type::FLOAT)
-                            {
-                                ark_object_v = Value(o.via.array.ptr[i].via.f64);
-                            }
-                            else if(o.via.array.ptr[i].type == type::STR)
-                            {
-                                ark_object_v = Value(std::string { o.via.array.ptr[i].via.str.ptr });
-                            }
-
-                            // TODO : sub list convert
-                            lst_v.push_back(ark_object_v);
-                        }
-
-                        v = lst_v;
+                        Value list_v  = Ark::Value(ValueType::List);
+                        convert_list(o.via.array, list_v);
+                        v = list_v;
                     }
 
                     return o;
@@ -80,32 +77,25 @@ namespace msgpack
                 {
 
                     if(v.valueType() == ValueType::Nil)
-                    {
                         o.pack_nil();
-                    }
                     else if(v.valueType() == ValueType::True)
-                    {
                         o.pack_true();
-                    }
                     else if(v.valueType() == ValueType::False)
-                    {
                         o.pack_false();
-                    }
                     else if(v.valueType() == ValueType::Number)
-                    {
                         o.pack_double(static_cast<Value>(v).number());
-                    }
                     else if(v.valueType() == ValueType::String)
                     {
-                        std::string str { static_cast<Value>(v).stringRef().toString() };
+                        std::string str = static_cast<Value>(v).string().toString();
                         o.pack_str(str.size());
                         o.pack_str_body(str.c_str(), str.size());
                     }
                     else
                     {
-                        std::vector<Value> list { static_cast<Value>(v).list() };
+                        std::vector<Value> list = static_cast<Value>(v).list();
                         o.pack_array(list.size());
-                        for(unsigned i = 0; i < list.size(); ++ i) { o.pack(list[i]); }
+                        for(std::size_t i = 0, end = list.size(); i < end; ++ i)
+                            o.pack(list[i]);
                     }
 
                     return o;
