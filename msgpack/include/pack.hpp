3b4d37695374b0e06f5a9f076e36dc8831ee9aa4
Pierre Pharel <edo.tensei095@gmail.com>
2021-07-23 18:51:11 +0200

    update include name file

--- msgpack/include/pack.hpp
+++ msgpack/include/pack.hpp
@@ -1,7 +1,7 @@
 #ifndef ARK_MSGPACK_PACK_HPP
 #define ARK_MSGPACK_PACK_HPP
 
-#include <object.hpp>
+#include <objects.hpp>
 
 Value msgpack_pack(std::vector<Value>& args, VM* vm);
 
--- msgpack/include/unpack.hpp
+++ msgpack/include/unpack.hpp
@@ -1,7 +1,7 @@
 #ifndef ARK_MSGPACK_UNPACK_HPP
 #define ARK_MSGPACK_UNPACK_HPP
 
-#include <object.hpp>
+#include <objects.hpp>
 
 Value msgpack_unpack(std::vector<Value>& args, VM* vm);
 Value msgpack_convert(std::vector<Value>& args, VM* vm);
